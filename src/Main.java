0674b8d7ce7ebce2b003c888f60d0a13529738ae
FelipeF27 <168601217+FelipeF27@users.noreply.github.com>
2025-03-05 17:10:23 -0300

    Implentação do método para verificar se um número é par

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Atividade Java!");
         System.out.println(soma(5, 10)); // Deve imprimir 15
+        System.out.println(ehPar(4)); // Deve imprimir true
+        System.out.println(ehPar(7)); // Deve imprimir false
+        
     }
 
     public static int soma(int a, int b) {
         return a + b;
+    }
+    public static boolean ehPar(int numero) {
+        return numero % 2 == 0;
     }
 }
